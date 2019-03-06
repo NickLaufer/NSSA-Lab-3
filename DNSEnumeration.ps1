1102066b47ce7ba56935ca8fb94af343ea8c8ac8
Nick <nxl6264@g.rit.edu>
2019-03-06 14:17:19 -0500

    Added DNS Enumeration tool and example file.

---
+++ DNSEnumeration.ps1
@@ -0,0 +1,14 @@
+﻿# DNS Enumeration Script
+# By Nick Laufer
+
+foreach($line in Get-Content ./hosts.txt){
+    try{
+        $DnsRecord = Resolve-DnsName $line -Type A -DNSOnly | Select Name, Namehost, IP4Address
+    }
+    catch{
+        Write-Host "Hostname $line could not be resolved."
+    }
+    Foreach($line in $DnsRecord){
+        $line
+    }
+}
